546d28e778527793dbda1b6210160cf4016710e3
David Kroupa <damodarak@mail.com>
2025-02-15 14:33:58 +0100

    create new project

---
+++ htn-transformator/htn-transformator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace htn_transformator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
